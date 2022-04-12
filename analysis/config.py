a58394acbc420b783379dd8bfe36b57ad03bb21a
Milan Wiedemann <milan.wiedemann@gmail.com>
2022-04-12 15:54:33 +0300

    match projec/yaml dates to config dates

--- analysis/config.py
+++ analysis/config.py
@@ -1,9 +1,9 @@
 # Study start date
 # Note: This should match the start dates in project.yaml
 # In QOF also: Payment Period Start Date (PPSD)
-start_date = "2019-01-01"
+start_date = "2020-04-01"
 
 # Study end date
 # Note: This should match the end dates in project.yaml
 # In QOF also: Payment Period End Date (PPED)
-end_date = "2022-01-01"
+end_date = "2021-03-01"
