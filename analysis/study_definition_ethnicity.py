39c3d005a263df9eddef7bb043b80b6cbc251583
Christine Cunningham <5705329+ccunningham101@users.noreply.github.com>
2022-04-21 08:19:05 -0700

    Do not supplement ethnicity with SUS
    We think those who have an unknown ethnicity but do have ethnicity in SUS
    may be a biased group. Until after the ethnicity short data report leave
    as a separate Unknown category

--- analysis/study_definition_ethnicity.py
+++ analysis/study_definition_ethnicity.py
@@ -33,11 +33,11 @@ study = StudyDefinition(
     ethnicity=patients.categorised_as(
         {
             "Unknown": "DEFAULT",
-            "White": "eth='1' OR (NOT eth AND ethnicity_sus='1')",
-            "Mixed": "eth='2' OR (NOT eth AND ethnicity_sus='2')",
-            "Asian": "eth='3' OR (NOT eth AND ethnicity_sus='3')",
-            "Black": "eth='4' OR (NOT eth AND ethnicity_sus='4')",
-            "Other": "eth='5' OR (NOT eth AND ethnicity_sus='5')",
+            "White": "eth='1'",
+            "Mixed": "eth='2'",
+            "Asian": "eth='3'",
+            "Black": "eth='4'",
+            "Other": "eth='5'",
         },
         eth=patients.with_these_clinical_events(
             ethnicity6_codes,
@@ -49,17 +49,6 @@ study = StudyDefinition(
                     "ratios": {"1": 0.2, "2": 0.2, "3": 0.2, "4": 0.2, "5": 0.2}
                 },
                 "incidence": 0.75,
-            },
-        ),
-        # fill missing ethnicity from SUS
-        ethnicity_sus=patients.with_ethnicity_from_sus(
-            returning="group_6",
-            use_most_frequent_code=True,
-            return_expectations={
-                "category": {
-                    "ratios": {"1": 0.2, "2": 0.2, "3": 0.2, "4": 0.2, "5": 0.2}
-                },
-                "incidence": 0.4,
             },
         ),
         return_expectations={
